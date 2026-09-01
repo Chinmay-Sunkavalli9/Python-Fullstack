a4ac9af9ab53e0962cebf8798cfcdc008fbeb150
chinmaysunkavalli19 <chinmaysunkavalli19@gmail.com>
2026-09-01 22:32:56 +0530

    Day 4 Python Practice

---
+++ Python-Full-Stack/01.Basics/practice/Day04.py
@@ -0,0 +1,52 @@
+# Set Example
+colors = {"Red", "Blue", "Green", "Red"}
+print(colors)
+print(type(colors))
+
+# Dictionary Example
+student = {
+    "name": "Chinmay",
+    "age": 20,
+    "course": "Python"
+}
+print(student)
+print(student["name"])
+print(student["course"])
+
+# Boolean Example
+a = 10
+b = 20
+print(a < b)
+print(a > b)
+
+# None Example
+value = None
+print(value)
+print(type(value))
+
+# Type Casting
+a = 25
+print(float(a))
+print(str(a))
+print(bool(a))
+
+# String to Integer
+num = "100"
+print(int(num))
+print(float(num))
+print(bool(num))
+
+# List to Tuple
+numbers = [10, 20, 30]
+print(list(colors))
+
+# String to List
+language = "Python"
+print(list(language))
+
+# Dictionary Conversion
+data = [
+    ("name", "Chinmay"),
+    ("batch", "Python")
+]
+print(dict(data))
