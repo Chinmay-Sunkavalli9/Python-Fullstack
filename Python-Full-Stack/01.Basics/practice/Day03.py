bee79e79bd358bf60bdd63798dfc8b0bdf56670c
chinmaysunkavalli19 <chinmaysunkavalli19@gmail.com>
2026-09-01 22:27:27 +0530

    Day 3 Python Basics Practice

---
+++ Python-Full-Stack/01.Basics/practice/Day03.py
@@ -0,0 +1,26 @@
+# List
+fruits = ["Apple", "Mango", "Orange"]
+print(fruits)
+print(fruits[1])
+
+# Tuple
+colors = ("Red", "Blue", "Green")
+print(colors)
+print(type(colors))
+
+# Range
+print(list(range(1, 6)))
+
+# Swapping
+a = 15
+b = 25
+a, b = b, a
+print(a, b)
+
+# Swapping (Using Third Variable)
+x = 50
+y = 100
+temp = x
+x = y
+y = temp
+print(x, y)
