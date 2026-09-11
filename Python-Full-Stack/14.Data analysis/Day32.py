c7ef6a96ac5af96faaee56bdb0eb04ceadea1e7d
chinmaysunkavalli19 <chinmaysunkavalli19@gmail.com>
2026-09-11 23:13:41 +0530

    Numpy

---
+++ Python-Full-Stack/14.Data analysis/Day32.py
@@ -0,0 +1,15 @@
+import numpy as np
+# 1D Array
+arr1 = np.array([1, 2, 3, 4, 5])
+print("1D Array:", arr1)
+# 2D Array
+arr2 = np.array([[1, 2, 3], [4, 5, 6]])
+print("2D Array:")
+print(arr2)
+# 3D Array
+arr3 = np.array([
+    [[1, 2], [3, 4]],
+    [[5, 6], [7, 8]]
+])
+print("3D Array:")
+print(arr3)
